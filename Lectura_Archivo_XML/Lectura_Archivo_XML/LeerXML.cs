22d3b9b355a011801dea9a939a7b1ad46838cb84
MarioBasilio08 <andres.basilio@outlook.es>
2023-06-12 11:05:57 -0600

    v1

---
+++ Lectura_Archivo_XML/Lectura_Archivo_XML/LeerXML.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Lectura_Archivo_XML
+{
+    public class LeerXML
+    {
+        private XmlDocument docXML;
+        public LeerXML(string filePath)
+        {
+            docXML = new XmlDocument();
+            try
+            {
+                docXML.Load(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cargar el archivo XML: " + ex.Message);
+            }
+        }
+
+        public void RecorrerXml()
+        {
+            XmlNode nodoRaiz = docXML.DocumentElement;
+            LeerNodo(nodoRaiz);
+        }
+
+        private void LeerNodo(XmlNode node, int depth = 0)
+        {
+            string identacion = new string(' ', depth * 4);
+            string nodoInfo = $"{identacion} Nodo: { node.Name} ";
+
+            if (node.Attributes != null && node.Attributes.Count > 0)
+            {
+                nodoInfo += " (Atributos: ";
+
+                foreach (XmlAttribute atributo in node.Attributes)
+                {
+                    nodoInfo += $"{atributo.Name} = {atributo.Value} , ";
+                }
+
+                nodoInfo = nodoInfo.TrimEnd(',', ' ') + ")";
+            }
+
+            Console.WriteLine(nodoInfo);
+
+            if (node.HasChildNodes)
+            {
+                foreach (XmlNode nodoHijo in node.ChildNodes)
+                {
+                    if (nodoHijo.NodeType == XmlNodeType.Element)
+                    {
+                        LeerNodo(nodoHijo, depth + 1);
+                    }
+                    else if (nodoHijo.NodeType == XmlNodeType.Text || nodoHijo.NodeType == XmlNodeType.CDATA)
+                    {
+                        Console.WriteLine($"{identacion} Contenido: {nodoHijo.Value.Trim()}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lectura_Archivo_XML/Lectura_Archivo_XML/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Lectura_Archivo_XML
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string rutaArchivo = "D:/Trabajo/Ejercicios/Configuraciones.xml";
+
+
+            LeerXML xmlReader = new LeerXML(rutaArchivo);
+            xmlReader.RecorrerXml();
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+}
